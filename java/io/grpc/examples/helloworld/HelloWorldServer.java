793d2b4b96e46102f60c38ed07ee2bb32c858f8b
Simon Horlick <simonhorlick@gmail.com>
2017-06-20 14:22:03 +0800

    Some more simplifications

--- java/io/grpc/examples/helloworld/HelloWorldServer.java
+++ java/io/grpc/examples/helloworld/HelloWorldServer.java
@@ -32,60 +32,20 @@
 package io.grpc.examples.helloworld;
 
 import dagger.grpc.server.NettyServerModule;
-import io.grpc.Server;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
 public class HelloWorldServer {
-  private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
 
-  private Server server;
-
-  private void start() throws IOException {
-    /* The port on which the server should run */
-    int port = 50051;
-    MyComponent component =
-        DaggerMyComponent.builder()
-            .nettyServerModule(NettyServerModule.bindingToPort(port))
-            .build();
-    server = component.server();
-    server.start();
-    logger.info("Server started, listening on " + port);
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        HelloWorldServer.this.stop();
-        System.err.println("*** server shut down");
-      }
-    });
-  }
-
-  private void stop() {
-    if (server != null) {
-      server.shutdown();
-    }
-  }
-
-  /**
-   * Await termination on the main thread since the grpc library uses daemon threads.
-   */
-  private void blockUntilShutdown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
-    }
-  }
-
-  /**
-   * Main launches the server from the command line.
-   */
   public static void main(String[] args) throws IOException, InterruptedException {
-    final HelloWorldServer server = new HelloWorldServer();
-    server.start();
-    server.blockUntilShutdown();
+    DaggerMyComponent.builder()
+            .nettyServerModule(NettyServerModule.bindingToPort(50051))
+            .build()
+            .server()
+            .start()
+            .awaitTermination();
   }
+
 }
--- java/io/grpc/examples/helloworld/MyModule.java
+++ java/io/grpc/examples/helloworld/MyModule.java
@@ -1,14 +1,29 @@
 package io.grpc.examples.helloworld;
 
 import dagger.Binds;
+import dagger.Component;
 import dagger.Module;
 import dagger.Provides;
 import dagger.grpc.server.ForGrpcService;
+import dagger.grpc.server.NettyServerModule;
+import io.grpc.Server;
 import io.grpc.ServerInterceptor;
 import io.grpc.examples.helloworld.interceptors.AuthInterceptor;
 import io.grpc.examples.helloworld.interceptors.LoggingInterceptor;
 import java.util.Arrays;
 import java.util.List;
+import javax.inject.Singleton;
+
+@Singleton
+@Component(modules = {
+    HelloUnscopedGrpcServiceModule.class,
+    MyModule.class,
+    NettyServerModule.class,
+})
+interface MyComponent extends HelloServiceDefinition {
+  /* Dagger implements this interface by providing a NettyServer. */
+  Server server();
+}
 
 @Module
 abstract class MyModule {
